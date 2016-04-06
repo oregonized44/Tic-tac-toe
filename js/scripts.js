59345f748bc05f47f98fb8a0cfa784b7a56a5abd
Connor Cummings and Cory Franks <cory@>
2016-04-06 16:02:24 -0700

    Added outline for additional functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 function Player(teamChoice, playerName){
   this.team = teamChoice; //x or o
-  this.user = playerName; //persons name so later we can say "" its your turn!
-}
-function Board(){
+  this.user = playerName; //persons name so later we can say "" its your turn
 
 }
+function BoardCombinations(a,b,c){
+  1,2,3
+  4,5,6
+  7,8,9
+  1,4,7
+  2,5,8
+  3,6,9
+  1,5,9
+  3,5,7
+
+}
+Player.prototype = {
+
+  Constructor: Player,
+
+  setTurn: function(firstName,secondName){
+    var firstName = userOne;
+    var secondName = userTwo;
+    firstName = "X";
+    secondName = "O";
+    turn = "X";
+    console.log(turn);
+  },
+
+
+}
+
+
 $(document).ready(function(){
   $("button#submit").click(function(event){
     event.preventDefault();
@@ -19,8 +45,15 @@ $(document).ready(function(){
     console.log(userTwo);
     $("#form").hide();
     $("#table").show();
+    setTurn(userOne,userTwo);
+
+
+
   });
 });
+
+
+
 /*
 function Board(){
 
